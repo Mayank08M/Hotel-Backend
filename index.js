3e9ffca4ad8137efee12f946a5c5449dc2288479
Mayank <mayank0806md@gmail.com>
2023-12-14 17:59:10 +0530

    connected database to the code

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-
+const mysql=require('mysql');
 app.use(cors())
+const connection = mysql.createConnection({
+    host: "15.206.6.55",
+    user:"hourlyrooms",
+    password:"Hourlyrooms@1584",
+    database:"mayankproject"
+});
+connection.connect((err)=>{
+    if(err){
+        throw err;
+    }
+    console.log('database is connected ')
+})
+
 
 app.get('/', (req, res) => {
-    res.send('Hello from our server!')
+    res.send('Hello')
+})
+app.get('/hotels', (req, res) => {
+    res.send('hii bro what are you doing')
 })
 
 app.listen(8000, () => {
-      console.log('server listening on port 8000')
+    console.log('server listening on port 8000')
 })
